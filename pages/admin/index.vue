87b8cf8c68b91ae9f2f722ceaa7eee9511300b7c
sunny96087 <sunny96087@yahoo.com.tw>
2024-05-26 16:01:49 +0800

    fix: 修正後台側選單開合問題

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -5,8 +5,8 @@ const barOpen = ref(false)
   <div class="flex min-h-screen">
     <!-- 左側選單 -->
     <div
-      class="hover-auto flex min-h-full w-[96px] min-w-[96px] flex-col items-center gap-5 bg-[#FFFBF8] px-2 py-6"
-      :class="{ 'w-[240px] min-w-[240px]': barOpen }"
+      class="hover-auto flex min-h-full w-[96px] shrink-0 flex-col items-center gap-5 bg-[#FFFBF8] px-2 py-6"
+      :class="{ 'min-w-[240px]': barOpen, 'min-w-[96px]': !barOpen }"
     >
       <div class="logo flex gap-2" @click="barOpen = !barOpen">
         <img src="~/assets/images/admin/slidebar/icon.svg" alt="logo" />
@@ -108,8 +108,8 @@ const barOpen = ref(false)
     </div>
 
     <!-- 頁面 -->
-    <div class="grow bg-[#F9F7F7] px-6 py-10 xl:px-8 xl:py-12">
-      <NuxtPage />
+    <div class="w-full grow bg-[#F9F7F7] px-6 py-10 xl:px-8 xl:py-12">
+      <NuxtPage class="w-full" />
     </div>
   </div>
 </template>
