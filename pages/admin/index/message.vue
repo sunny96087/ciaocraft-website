cf037bf9d0b756ee1e547aeb2365622c5e6f7204
sunny96087 <sunny96087@yahoo.com.tw>
2024-05-31 21:37:06 +0800

    feat: 新增取得 IP api
    1. 新增取得 IP api
    2. 範例放在 admin / message

--- pages/admin/index/message.vue
+++ pages/admin/index/message.vue
@@ -1,5 +1,32 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { APIStore } from '~/stores/apiService'
+const store = APIStore()
+import { showToast, openDialog, showLoading, hideLoading } from '~/stores/eventBus'
+
+const userIP: any = ref('')
+
+onMounted(() => {
+  getUserIP()
+})
+
+// NOTE 取得使用者IP
+async function getUserIP() {
+  try {
+    showLoading()
+
+    const res = await store.apiGetUserIp()
+    const result = res.data
+    console.log(`getUserIP result = ${JSON.stringify(result)}`)
+
+    userIP.value = result.ip
+  } catch (e) {
+    console.log(e)
+  } finally {
+    hideLoading()
+  }
+}
+</script>
 <template>
-  <div class="">message</div>
+  <div class="">{{ userIP }}</div>
 </template>
 <style scoped></style>
