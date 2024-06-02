a5a59e7e1aa7bc27ab1a2445919aceab0326ff74
sunny96087 <sunny96087@yahoo.com.tw>
2024-06-02 14:56:43 +0800

    feat: 新增 前台 申請成為賣家頁面

---
+++ pages/index/index/vendorApply.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { APIStore } from '~/stores/apiService'
+const store = APIStore()
+import { showToast, openDialog, showLoading, hideLoading } from '~/stores/eventBus'
+</script>
+<template>
+  <div class="">vendorApply</div>
+</template>
+<style scoped></style>
