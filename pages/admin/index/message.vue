7f970fc1d72eca8d9577f83c8b2423b02b67996a
sunny96087 <sunny96087@yahoo.com.tw>
2024-06-04 21:03:30 +0800

    feat: 後台 訊息中心 手機版樣式
    1. 採用全畫面斷點完全切換

--- pages/admin/index/message.vue
+++ pages/admin/index/message.vue
@@ -230,11 +230,145 @@ watch(messages, () => {
 })
 
 onUpdated(scrollToBottom)
+
+// NOTE ref 重複會衝突，所以手機改用 messageListM
+const messageListM: any = ref(null)
+const scrollToBottomM = () => {
+  nextTick(() => {
+    const listElement = messageListM.value
+    if (listElement) {
+      listElement.scrollTop = listElement.scrollHeight
+    }
+  })
+}
+
+watch(messages, () => {
+  scrollToBottomM()
+})
+
+onUpdated(scrollToBottomM)
 </script>
 
 <template>
+  <!-- * mb -->
+  <div class="relative h-[90vh] items-start gap-[30px] overflow-hidden lg:hidden">
+    <!-- * mb 聊天室列表 -->
+    <div class="m-message-list" v-if="rooms.length">
+      <div class="message-list-title">訊息中心</div>
+      <div class="admin-select mb-7 flex">
+        <input type="text" class="w-full" v-model="currentKeyword" placeholder="用戶名稱" />
+        <button class="">
+          <Icon name="fluent:search-48-filled" size="24" class="text-dark3"></Icon>
+        </button>
+      </div>
+
+      <div class="max-h-full w-full overflow-y-auto">
+        <div
+          v-for="item in rooms"
+          :key="item._id"
+          @click="selectRoom(item)"
+          :class="['message-list-item', { 'selected-room': roomId === item._id }]"
+          class="flex gap-3"
+        >
+          <div
+            v-if="currentMemberInfo.photo"
+            class="h-[44px] min-h-[44px] w-[44px] min-w-[44px] overflow-hidden rounded-full"
+          >
+            <img :src="currentMemberInfo?.photo" alt="" class="pic-auto" />
+          </div>
+          <Icon
+            v-else
+            size="44"
+            class="min-w-[44px] text-gray4"
+            name="material-symbols:account-circle-full"
+          />
+          <div class="grow">
+            <div class="">{{ item._id }}</div>
+            <div class="">{{ item.memberId?.name }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- * mb 聊天框 -->
+    <div class="m-message-block" v-if="messages.length">
+      <!-- ? 測試資料 -->
+      <!-- <div>
+        <p>
+          會員ID: <span id="memberId">{{ memberId }}</span>
+        </p>
+        <p>
+          廠商ID: <span id="vendorId">{{ vendorId }}</span>
+        </p>
+        <p>
+          房間ID: <span id="roomId">{{ roomId }}</span>
+        </p>
+      </div> -->
+
+      <!-- ? 用戶資料 -->
+      <div class="flex items-center gap-3 border-b border-[#DFE4EA] p-4" v-if="currentMemberInfo">
+        <div v-if="currentMemberInfo.photo" class="h-[52px] w-[52px] overflow-hidden rounded-full">
+          <img :src="currentMemberInfo?.photo" alt="" class="pic-auto" />
+        </div>
+        <Icon v-else size="52" class="text-gray4" name="material-symbols:account-circle-full" />
+
+        <div class="grow">{{ currentMemberInfo.name }}</div>
+
+        <button @click="closeChatRoom">
+          <Icon name="material-symbols:close-rounded" size="24" class="text-gray"></Icon>
+        </button>
+      </div>
+
+      <!-- ? 訊息 -->
+      <div id="list" ref="messageListM" class="flex h-full grow flex-col gap-3 overflow-y-auto p-5">
+        <div v-for="(message, index) in messages" :key="index">
+          <div v-if="message.memberId">
+            <div class="text-[14px] text-text-secondary">{{ currentMemberInfo.name }}</div>
+            <div class="message left">
+              <div class="left-context">{{ message.content }}</div>
+            </div>
+            <div class="message-date">{{ message.timestamp }}</div>
+          </div>
+          <div v-else class="message right">
+            <div class="right-content">
+              {{ message.content }}
+            </div>
+            <div class="message-date">{{ message.timestamp }}</div>
+          </div>
+        </div>
+      </div>
+
+      <!-- ? 輸入框 -->
+      <div class="flex gap-5 border-t border-[#DFE4EA] p-5">
+        <input
+          type="text"
+          v-model="inputVendorMessage"
+          class="w-full rounded-md border border-gray5 bg-gray2 px-3 py-2"
+          placeholder="輸入訊息..."
+        />
+        <button
+          :disabled="!inputVendorMessage"
+          type="button"
+          id="vendorSendBtn"
+          class="btn-orange shrink-0"
+          @click="sendVendorMessage"
+        >
+          送出
+        </button>
+      </div>
+
+      <!-- <input
+        type="text"
+        id="memberMessage"
+        v-model="inputMemberMessage"
+        placeholder="會員輸入訊息"
+      />
+      <button type="button" id="memberSendBtn" @click="sendMemberMessage">會員送出</button> 
+    --></div>
+  </div>
+  <!-- * pc -->
   <div class="hidden h-[90vh] items-start gap-[30px] overflow-hidden lg:flex">
-    <!-- * 聊天室列表 -->
+    <!-- * pc 聊天室列表 -->
     <div class="message-list" v-if="rooms.length">
       <div class="message-list-title">訊息中心</div>
       <div class="admin-select mb-7 flex">
@@ -244,20 +378,35 @@ onUpdated(scrollToBottom)
         </button>
       </div>
 
-      <div class="max-h-full overflow-auto">
+      <div class="max-h-full w-full">
         <div
           v-for="item in rooms"
           :key="item._id"
           @click="selectRoom(item)"
           :class="['message-list-item', { 'selected-room': roomId === item._id }]"
+          class="flex gap-3"
         >
-          <div class="">{{ item._id }}</div>
-          <div class="">{{ item.memberId?.name }}</div>
+          <div
+            v-if="currentMemberInfo.photo"
+            class="h-[44px] min-h-[44px] w-[44px] min-w-[44px] overflow-hidden rounded-full"
+          >
+            <img :src="currentMemberInfo?.photo" alt="" class="pic-auto" />
+          </div>
+          <Icon
+            v-else
+            size="44"
+            class="min-w-[44px] text-gray4"
+            name="material-symbols:account-circle-full"
+          />
+          <div class="grow">
+            <div class="">{{ item._id }}</div>
+            <div class="">{{ item.memberId?.name }}</div>
+          </div>
         </div>
       </div>
     </div>
 
-    <!-- * 聊天框 -->
+    <!-- * pc 聊天框 -->
     <div class="message-block" v-if="messages.length">
       <!-- ? 測試資料 -->
       <!-- <div>
@@ -336,8 +485,20 @@ onUpdated(scrollToBottom)
 </template>
 
 <style scoped>
+/* NOTE mb */
+
+.m-message-list {
+  @apply w-full overflow-y-auto rounded-lg bg-white p-5;
+  box-shadow: 0px 1px 3px 0px #a6afc366;
+}
+
+.m-message-block {
+  @apply fixed inset-0 z-[999] flex grow flex-col rounded-lg border border-[#DFE4EA] bg-white;
+}
+
+/* NOTE pc */
 .message-list {
-  @apply w-[320px] max-w-[320px] overflow-auto rounded-lg bg-white p-5;
+  @apply w-[360px] max-w-[360px] overflow-y-auto rounded-lg bg-white p-5;
   box-shadow: 0px 1px 3px 0px #a6afc366;
 }
 
@@ -346,7 +507,7 @@ onUpdated(scrollToBottom)
 }
 
 .message-list-item {
-  @apply cursor-pointer px-4 py-2;
+  @apply cursor-pointer border-b border-gray3 px-4 py-2;
 }
 
 .message-block {
