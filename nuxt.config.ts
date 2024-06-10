34030f7db11f87cf19d84d2ca45b4385523e62b7
sunny96087 <sunny96087@yahoo.com.tw>
2024-06-10 21:10:46 +0800

    fix: vercel 崩潰問題 > 修不好

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,20 +2,5 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'nuxt-swiper', 'nuxt-icon', '@pinia/nuxt'],
-  css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      {
-        name: 'load-svg',
-        enforce: 'pre',
-        transform(src, id) {
-          if (id.endsWith('.svg')) {
-            const svgExport = `const content = ${JSON.stringify(src)};
-              export default content;`
-            return svgExport
-          }
-        }
-      }
-    ]
-  }
+  css: ['~/assets/css/main.css']
 })
