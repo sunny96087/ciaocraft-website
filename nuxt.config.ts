d0cc8d91cc18f023e230807f910258aa59150bc7
sunny96087 <sunny96087@yahoo.com.tw>
2024-06-10 20:58:43 +0800

    fix: 修正 icon plugin 導致 vercel 崩潰問題

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,17 +6,14 @@ export default defineNuxtConfig({
   vite: {
     plugins: [
       {
-        name: 'custom-phosphor-icons-handler',
-        apply: 'build',
-        transform(code, id) {
-          // 检查模块ID是否包含特定的路径
-          if (id.includes('node_modules/@phosphor-icons/vue')) {
-            // 这里进行您需要的代码转换或修改
-            // 例如，将导入语句改写为动态导入
-            return `export const icon = ${JSON.stringify(code)};`
+        name: 'load-svg',
+        enforce: 'pre',
+        transform(src, id) {
+          if (id.endsWith('.svg')) {
+            const svgExport = `const content = ${JSON.stringify(src)};
+              export default content;`
+            return svgExport
           }
-          // 如果不匹配，则返回原始代码
-          return code
         }
       }
     ]
