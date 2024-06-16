5456e936869a89a5db19ec7ec96930c644f802f0
JennyDisc <touki741@hotmail.com>
2024-06-16 21:03:31 +0800

    feat:前端首頁增加連結(底部關於我們)

--- components/front/About.vue
+++ components/front/About.vue
@@ -2,14 +2,14 @@
 
 <template>
   <div
-    class="bg-gray3 rounded-2xl bg-[url('~/assets/images/front/95e2fdb027fab5bc58110027b7b7bf7a.png')] py-[30px] pl-[30px] pr-[30px] lg:pr-0"
+    class="rounded-2xl bg-gray3 bg-[url('~/assets/images/front/95e2fdb027fab5bc58110027b7b7bf7a.png')] py-[30px] pl-[30px] pr-[30px] lg:pr-0"
   >
     <h2 class="mb-5 text-3xl font-medium leading-[38px]">關於我們</h2>
     <p class="mb-5 md:mb-[44px] lg:w-6/12">
       歡迎來到「巧手玩藝」，一個專為熱愛創作與學習手作技藝的您打造的社群平台！我們致力於連結具有獨特專業技能的品牌賣家與渴望學習的學員，為您提供一個豐富多元的手作體驗空間。不論您是想要尋找生活中的樂趣、提升專業技能，抑或是發展斜槓事業，「巧手玩藝」都能滿足您的需求。
     </p>
-    <button class="bg-primary hover:bg-primary-light rounded px-6 py-[9px] text-white">
-      了解更多
+    <button class="rounded bg-primary px-6 py-[9px] text-white hover:bg-primary-light">
+      <NuxtLink to="/about">了解更多</NuxtLink>
     </button>
   </div>
 </template>
